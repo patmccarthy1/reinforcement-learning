f8fc64bc256f7b7956076819d85183ccd8b8d1ff
patmccarthy1 <pat.mccarthy1@aol.com>
2020-11-29 16:45:36 +0000

    added updated custom DQN

---
+++ DQN_custom/final/agent.py
@@ -0,0 +1,316 @@
+############################################################################
+############################################################################
+# THIS IS THE ONLY FILE YOU SHOULD EDIT
+#
+#
+# Agent must always have these five functions:
+#     __init__(self)
+#     has_finished_episode(self)
+#     get_next_action(self, state)
+#     set_next_state_and_distance(self, next_state, distance_to_goal)
+#     get_greedy_action(self, state)
+#
+#
+# You may add any other functions as you wish
+############################################################################
+############################################################################
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import numpy as np
+import torch
+import copy
+import collections
+import matplotlib.pyplot as plt
+
+class Agent:
+
+    # Function to initialise the agent
+    def __init__(self,EPISODE_LENGTH, MINIBATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE):
+        # Set the episode length
+        self.episode_length = EPISODE_LENGTH
+        # Reset the total number of steps which the agent has taken
+        self.num_steps_taken = 0
+        # The state variable stores the latest state of the agent in the environment
+        self.state = None
+        # The action variable stores the latest action which the agent has applied to the environment
+        self.action = None
+        # create DQN
+        self.dqn = DQN()
+        # create replay buffer
+        self.buffer = ReplayBuffer()
+        # set minibatch size
+        self.minibatch_size = MINIBATCH_SIZE
+        # create list to store loss across episode
+        self.loss_list = []
+        # variable to record whether goal state reached
+        self.finished_training = False
+        # discount factor
+        self.discount = DISCOUNT
+        # epsilon decay
+        self.epsilon_decay = EPSILON_DECAY
+        # how often to update target network (number of episodes)
+        self.target_update = TARGET_UPDATE
+        
+    # Function to check whether the agent has reached the end of an episode
+    def has_finished_episode(self):
+        if self.num_steps_taken % self.episode_length == 0:
+            return True
+        else:
+            return False
+
+    # Function to get the next action, using whatever method you like
+    def get_next_action(self, state):
+        # Here, the action is random, but you can change this
+        action = self.get_greedy_action(state) # get greedy action
+        # Update the number of steps which the agent has taken
+        self.num_steps_taken += 1
+        # Store the state; this will be used later, when storing the transition
+        self.state = state
+        # Store the action; this will be used later, when storing the transition
+        self.action = action
+        
+        return action
+
+    # Function to set the next state and distance, which resulted from applying action self.action at state self.state
+    def set_next_state_and_distance(self, next_state, distance_to_goal):
+        # Convert the distance to a reward
+        # if np.all(next_state == self.state): # punish if at boundary
+        #     reward = -1.5
+        # else:
+        
+        reward = - distance_to_goal**0.3 
+        
+        if distance_to_goal <= 0.03:
+            reward = 1
+            
+        # Create a transition
+        transition = (self.state, self._continuous_action_to_discrete(self.action), reward, next_state) # get transition tuple, converting continuous action to discrete 
+        
+        episode_num = np.floor(self.num_steps_taken/self.episode_length)
+        
+        # if reached the goal
+        if distance_to_goal <= 0.03 and episode_num % 10 == 0:
+            self.finished_training == True
+            # print('greedy policy leads to goal state - training stopped')
+        
+        self.buffer.append_transition_tuple(transition)        
+
+        # print('Transition: ', transition)
+        
+        # create tensor to store loss
+        loss = torch.tensor(0)
+        
+        if self.num_steps_taken >= self.minibatch_size and not self.finished_training:
+            loss = self.dqn.train_q_network(self.buffer, self.minibatch_size, self.discount) # train network 
+            
+        number_of_episodes = np.floor(self.num_steps_taken/self.episode_length)
+        if number_of_episodes% self.target_update == 0:
+            self.dqn.update_target_network() # update target network every 5 episodes
+        
+        return loss.item()
+    
+    # Function to get the greedy action for a particular state
+    def get_greedy_action(self, state):
+        
+        epsilon_decay = self.epsilon_decay
+        
+        episode_num = np.floor(self.num_steps_taken/self.episode_length)
+        num_steps_left_in_episode = self.episode_length - self.num_steps_taken%self.episode_length
+        epsilon = epsilon_decay ** (episode_num) + episode_num * num_steps_left_in_episode/10e5
+                
+        state = torch.tensor(state)
+        
+        predictions = self.dqn.q_network.forward(torch.unsqueeze(state, 0))[0]
+        greedy_action = int(torch.argmax(predictions,0))
+        predictions = torch.cat([predictions[0:greedy_action], predictions[greedy_action + 1: ]])
+        next_greedy_action = int(torch.argmax(predictions,0))
+        
+        num = np.random.random()        
+        
+        # if num > epsilon:
+        #     action = greedy_action
+        # else:
+        #     action = np.random.choice([0,1,2,3])
+        
+        # every 10 episodes, set to greedy actions
+        if episode_num % 10 == 0 and episode_num!=0:
+            epsilon = 0
+            # print('greedy action taken')
+        
+        # if finished training (because greedy policy leads to goal state) set epsilon to zero so agent continuous to take this for the rest of the training period
+        if self.finished_training == True:
+            epsilon = 0
+            # print('finished training')
+                            
+        # graded epsilon-greedy
+        if num > epsilon:
+            action = greedy_action
+        elif num > epsilon + (1-epsilon)/3:
+            action = next_greedy_action
+        else:
+            action = np.random.choice([0,1,2])
+                
+        action = self._discrete_action_to_continuous(action) # convert action back to continous terms since this is how it will be treated in train_and_test.py
+        
+        return action
+    
+    # function to convert continuous action to discrete 
+    # 0 = up, 1 = right, 2 = down, 3 = left
+    def _continuous_action_to_discrete(self,continuous_action):
+        if np.all(continuous_action == np.array([0,0.02],dtype=np.float32)): # Move up
+            discrete_action = 0
+        elif np.all(continuous_action == np.array([0.02,0],dtype=np.float32)): # Move right
+            discrete_action = 1
+        elif np.all(continuous_action == np.array([0,-0.02],dtype=np.float32)): # Move down
+            discrete_action = 2
+        # elif np.all(continuous_action == np.array([0.02*np.cos(np.pi/4),0.02*np.cos(np.pi/4)],dtype=np.float32)): # Move up and right
+        #     discrete_action = 3
+        # elif np.all(continuous_action == np.array([0.02*np.cos(np.pi/4),-0.02*np.cos(np.pi/4)],dtype=np.float32)): # Move down and right
+        #     discrete_action = 4
+        return discrete_action
+    
+    # function to convert discrete action to continous
+    # 0 = up, 1 = right, 2 = down, 3 = left
+    def _discrete_action_to_continuous(self, discrete_action):
+        if discrete_action == 0: # Move up
+            continuous_action = np.array([0, 0.02], dtype=np.float32)
+        elif discrete_action == 1: # Move right
+            continuous_action = np.array([0.02,0], dtype=np.float32)
+        elif discrete_action == 2: # Move down
+            continuous_action = np.array([0,-0.02], dtype=np.float32)
+        # elif discrete_action == 3: # Move up and right
+        #     continuous_action = np.array([0.02*np.cos(np.pi/4),0.02*np.cos(np.pi/4)],dtype=np.float32)
+        # elif discrete_action == 4: # Move down and right 
+        #     continuous_action = np.array([0.02*np.cos(np.pi/4),-0.02*np.cos(np.pi/4)],dtype=np.float32)
+        return continuous_action
+    
+    # # function to plot loss for analysis
+    # def plot_loss(self):
+    #     if self.num_steps_taken >= self.minibatch_size:
+    #         loss = self.dqn.train_q_network(self.buffer, self.minibatch_size, 0.95) # train network and return loss
+    #         self.loss_list.append(loss)
+    #         if self.num_steps_taken % 50 == 0:
+    #             plt.plot(range(self.num_steps_taken),self.loss_list)
+    #             plt.x_label('number of steps')
+    #             plt.y_label('average loss')
+    #             plt.show()
+
+# The Network class inherits the torch.nn.Module class, which represents a neural network.
+class Network(torch.nn.Module):
+
+    # The class initialisation function. This takes as arguments the dimension of the network's input (i.e. the dimension of the state), and the dimension of the network's output (i.e. the dimension of the action).
+    def __init__(self, input_dimension, output_dimension):
+        # Call the initialisation function of the parent class.
+        super(Network, self).__init__()
+        # Define the network layers. This example network has two hidden layers, each with 100 units.
+        self.layer_1 = torch.nn.Linear(in_features=input_dimension, out_features=100)
+        self.layer_2 = torch.nn.Linear(in_features=100, out_features=100)
+        self.output_layer = torch.nn.Linear(in_features=100, out_features=output_dimension)
+
+    # Function which sends some input data through the network and returns the network's output. In this example, a ReLU activation function is used for both hidden layers, but the output layer has no activation function (it is just a linear layer).
+    def forward(self, input):
+        layer_1_output = torch.nn.functional.relu(self.layer_1(input))
+        layer_2_output = torch.nn.functional.relu(self.layer_2(layer_1_output))
+        output = self.output_layer(layer_2_output)
+        return output
+    
+# The DQN class determines how to train the above neural network.
+class DQN:
+
+    # The class initialisation function.
+    def __init__(self):
+        # Create a Q-network, which predicts the q-value for a particular state.
+        self.q_network = Network(input_dimension=2, output_dimension=3)
+        # Define the optimiser which is used when updating the Q-network. The learning rate determines how big each gradient step is during backpropagation.
+        self.optimiser = torch.optim.Adam(self.q_network.parameters(), lr=0.002)
+        # create target network object
+        self.target_network = copy.deepcopy(self.q_network)
+
+    # Function that is called whenever we want to train the Q-network. Each call to this function takes in a transition tuple containing the data we use to update the Q-network.
+    def train_q_network(self, buffer, minibatch_length, discount):
+        # Set all the gradients stored in the optimiser to zero.
+        self.optimiser.zero_grad()
+        # sample minibatch
+        minibatch = buffer.sample_minibatch(buffer.get_length(), minibatch_length)
+        # Calculate the loss for this transition.
+        loss = self._calculate_minibatch_loss(minibatch, discount)
+        # Compute the gradients based on this loss, i.e. the gradients of the loss with respect to the Q-network parameters.
+        loss.backward()
+        # Take one gradient step to update the Q-network.
+        self.optimiser.step()
+        # Return the loss as a scalar
+        return loss
+
+    
+    # function to calculate loss of minibatch of transitions to train Q network
+    def _calculate_minibatch_loss(self, minibatch, discount):
+        
+        # create lists to hold transition variables for transitions in minibatch
+        rewards = []
+        minibatch_states = []
+        minibatch_actions = []
+        minibatch_next_states = []
+        
+        # append values for each transition to their respective lists
+        for i in range(len(minibatch)):
+            rewards.append(minibatch[i][2])
+            minibatch_states.append(minibatch[i][0])
+            minibatch_actions.append(minibatch[i][1])
+            minibatch_next_states.append(minibatch[i][3])
+            
+        # convert lists to PyTorch tensors
+        rewards_tensor = torch.tensor(rewards,dtype=torch.float32)
+        minibatch_states_tensor = torch.tensor(minibatch_states)
+        minibatch_actions_tensor = torch.tensor(minibatch_actions)
+        minibatch_next_states_tensor = torch.tensor(minibatch_next_states)
+        
+        # next state Q-values for use in Bellman equation - Double Deep Q Network
+        # idx = torch.argmax(self.target_network.forward(minibatch_next_states_tensor),1) # use target network to find action with highest Q value
+        # max_next_state_Q_values = self.q_network.forward(minibatch_next_states_tensor).gather(dim=1, index=idx.long().unsqueeze(-1)).squeeze(-1)# calculate Q-value for action with highest Q-value using Q-network (Double Deep Q Network)
+    
+        # no Double Deep Q Learning
+        max_next_state_Q_values, idx = torch.max(self.target_network.forward(minibatch_next_states_tensor),1) # target network
+        max_next_state_Q_values = max_next_state_Q_values.detach()
+        
+        # predict Q-values
+        Q_predictions = self.q_network.forward(minibatch_states_tensor).gather(dim=1, index=minibatch_actions_tensor.long().unsqueeze(-1)).squeeze(-1)
+        
+        # measure change in Q-value compared to previous
+        
+        # use Bellman equation to calculate actual Q values
+        Q_actual = rewards_tensor + discount*max_next_state_Q_values
+        
+        
+        loss = torch.nn.MSELoss()(Q_predictions, Q_actual)
+        
+        return loss
+    
+    # function to update target network by copying weights from main network when called
+    def update_target_network(self):
+        self.target_network = copy.deepcopy(self.q_network)
+        
+    
+# REPLAY BUFFER CLASS FOR MINIBATCH TRAINING
+class ReplayBuffer():
+    
+    def __init__(self):  
+        self.buffer = collections.deque(maxlen=5000)
+        
+    def append_transition_tuple(self, transition_tuple):
+        self.buffer.append(transition_tuple)
+        
+    def sample_minibatch(self, total_transitions, minibatch_size):
+        minibatch_indices = np.random.choice(range(total_transitions), minibatch_size, replace=False) # replace = False so no repeated elements, otherwise minibatch size may be <50 due to way we're defining it
+        # enumerate deque and create list of tuples according to randomly generated minibatch indices         
+        minibatch = [transition_tuple for i, transition_tuple in enumerate(self.buffer) if i in minibatch_indices]
+        return minibatch
+        
+    def get_length(self):
+        return len(self.buffer)
+    
+    
+    
+    
+    
---
+++ DQN_custom/final/train_and_test.py
@@ -0,0 +1,120 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import time
+import numpy as np
+
+from random_environment import Environment
+from agent import Agent
+
+import matplotlib.pyplot as plt
+
+def main(EPISODE_LENGTH, MINIBATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE):
+    
+    # This determines whether the environment will be displayed on each each step.
+    # When we train your code for the 10 minute period, we will not display the environment.
+    display_on = True
+
+    # Create a random seed, which will define the environment
+    # random_seed = int(time.time())
+    random_seed = 1606237025
+    np.random.seed(random_seed)
+
+    # Create a random environment
+    environment = Environment(magnification=500)
+
+    # Create an agent
+    agent = Agent(EPISODE_LENGTH, MINIBATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE)
+
+    # keep track of episode
+    episode = 0
+    
+    # Get the initial state
+    state = environment.init_state
+
+    # Determine the time at which training will stop, i.e. in 10 minutes (600 seconds) time
+    start_time = time.time()
+    end_time = start_time + 600
+
+    loss_list = []
+    ave_loss_list = []
+    
+    # fig = plt.gcf()
+    # fig.show()
+    # fig.canvas.draw()
+        
+    print('EPISODE_LENGTH: {}\nMINIBATCH_SIZE: {}\nDISCOUNT: {}\nEPSILON_DECAY: {}\nTARGET_UPDATE: {}'.format(EPISODE_LENGTH, MINIBATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE))
+
+    # Train the agent, until the time is up
+    while time.time() < end_time:
+        # If the action is to start a new episode, then reset the state
+        if agent.has_finished_episode():
+            time_passed = time.time() - start_time
+            episode += 1
+            # mins = int(np.floor(time_passed/60))
+            # secs = int(time_passed % 60)
+            # print('Episode: ', episode)
+            # print('Time passed: {} mins {} secs'.format(mins,secs))
+            ave_loss = np.mean(loss_list)
+            ave_loss_list.append(ave_loss)
+            # plt.plot(range(episode),ave_loss_list)
+            # plt.xlabel('episode number')
+            # plt.ylabel('average loss')
+            # plt.yscale('log')
+            # plt.title('average loss vs episode number (log scale)')
+            state = environment.init_state
+            # fig.canvas.draw()
+            # plt.pause(0.0001)
+            loss_list = []
+        
+        # Get the state and action from the agent
+        action = agent.get_next_action(state)
+        # Get the next state and the distance to the goal
+        next_state, distance_to_goal = environment.step(state, action)
+        # Return this to the agent
+        loss = agent.set_next_state_and_distance(next_state, distance_to_goal)
+        loss_list.append(loss)
+        # Set what the new state is
+        state = next_state
+        # Optionally, show the environment
+        if display_on:
+            environment.show(state)
+    
+    print('FINISHED')
+    
+    plt.plot(range(episode),ave_loss_list)
+    plt.xlabel('episode number')
+    plt.ylabel('average loss')
+    plt.yscale('log')
+    plt.title('average loss vs episode number (log scale)')   
+    plt.savefig('hyperparameter_tests/loss_eplen{}_mbsize{}_disc{}_epdec{}_targup{}.png'.format(EPISODE_LENGTH, MINIBATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE),dpi=1000)
+    plt.close()
+    
+    policy = []
+    
+    # Test the agent for 100 steps, using its greedy policy
+    state = environment.init_state
+    policy.append(state)
+    has_reached_goal = False
+    for step_num in range(100):
+        action = agent.get_greedy_action(state)
+        next_state, distance_to_goal = environment.step(state, action)
+        policy.append(next_state)
+        # The agent must achieve a maximum distance of 0.03 for use to consider it "reaching the goal"
+        if distance_to_goal < 0.03:
+            has_reached_goal = True
+            break
+        state = next_state
+    
+    environment.draw_policy(policy,EPISODE_LENGTH, MINIBATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE)
+    
+    # Print out the result
+    if has_reached_goal:
+        print('Reached goal in ' + str(step_num) + ' steps.')
+    else:
+        print('Did not reach goal. Final distance = ' + str(distance_to_goal))
+
+if __name__ == "__main__":
+    import sys
+    main(EPISODE_LENGTH, BATCH_SIZE, DISCOUNT, EPSILON_DECAY, TARGET_UPDATE)
+    
